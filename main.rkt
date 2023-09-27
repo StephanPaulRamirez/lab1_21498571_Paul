c0c184ae740f01bd9e61f62d7873ac609a32ff54
Stephan Paul <stephan.paul@usach.cl>
2023-09-27 20:33:06 -0300

    se agrego la funcion chatbot y agregar flow a chatbot

--- main.rkt
+++ main.rkt
@@ -5,12 +5,12 @@
 ; Recorrido: option
 ; Recursión: ninguna
 ; Descripción: Esta funcion toma los primeros parametros del dominio y los enlista
-;              (siendo estos el id, el mensaje y los id del flow y el chatbot) junto con hacer
-;              Append a esta misma con la lista de la cantidad indefinida de keywords con el 
+;              siendo estos el id, el mensaje, los id del flow y el chatbot, y 
+;              la lista de la cantidad indefinida de keywords con el 
 ;              fin de tener todos los argumentos en una sola lista y la retorna.
 
 (define option (lambda (code message ChatbotCodeLink InitialFlowCodeLink . Keyword)
-                 (append (list code message ChatbotCodeLink InitialFlowCodeLink) Keyword)))
+                 (list code message ChatbotCodeLink InitialFlowCodeLink Keyword)))
 
 
 ; Nombre de la funcion: option-get-id
@@ -36,25 +36,27 @@
 ;              su id a id-list en la llamada recursiva sin el primer elemento nuevamente.
 
 (define flow-remove-dup-envoltorio (lambda (options)
-                        (define option-remove-dup (lambda (result id-list options)
-                            (if (null? options)
-                                result                                
-                                (if (member (option-get-id (car options)) id-list)
-                                    (option-remove-dup result id-list (cdr options))
-                                    (option-remove-dup (append result (list (car options))) (cons (option-get-id (car options)) id-list) (cdr options))))))
-                        (option-remove-dup (list) (list) options)))
+                (define option-remove-dup (lambda (result id-list options)
+                    (if (null? options)
+                        result                                
+                        (if (member (option-get-id (car options)) id-list)
+                            (option-remove-dup result id-list (cdr options))
+                            (option-remove-dup (append result (list (car options)))
+                                               (cons (option-get-id (car options)) id-list)
+                                               (cdr options))))))
+                (option-remove-dup (list) (list) options)))
 
 
 ; Nombre de la funcion: flow
 ; Dominio: int X string X list
 ; Recorrido: flow
 ; Recursión: ninguna
-; Descripción: Esta funcion enlista los primeros 2 parametros (el id y el mensaje) y luego hace append con
-;              la lista de opciones luego de componerse con la funcion "flow-remove-dup-envoltorio"
+; Descripción: Esta funcion enlista los primeros 2 parametros (el id y el mensaje)
+;              con la lista de opciones luego de componerse con la funcion "flow-remove-dup-envoltorio"
 ;              con el fin de que solo se agreguen los option sin repetir id.
 
 (define flow (lambda (id name-msg . Option)
-               (append (list id name-msg) (flow-remove-dup-envoltorio Option))))
+               (list id name-msg (flow-remove-dup-envoltorio Option))))
 
 
 ; Nombre de la funcion: check-dup-flow
@@ -95,16 +97,53 @@
 ; Recursión: ninguna
 ; Descripción: Esta funcion retorna las opciones de un flujo con el uso de cddr.
 
-(define flow-get-options (lambda (flow) (cddr flow)))
+(define flow-get-options (lambda (flow) (caddr flow)))
 
 
 ; Nombre de la funcion: flow-add-option
 ; Dominio: flow X option
 ; Recorrido: flow
 ; Recursión: ninguna
-; Descripción: Esta funcion enlista los 2 primeros elementos del flujo y luego los agrega a la lista de
+; Descripción: Esta funcion enlista los 2 primeros elementos del flujo con la lista de
 ;              opciones con la opcion nueva, la cual se procesa con la funcion "check-dup-flow", para revisar
 ;              si efectivamente se agrega la nueva opcion o no.
 
 (define flow-add-option (lambda (flow option)
-                         (append (list (flow-get-id flow) (flow-get-msg flow)) (check-dup-flow (flow-get-options flow) option))))
+                         (list (flow-get-id flow) (flow-get-msg flow)
+                               (check-dup-flow (flow-get-options flow) option))))
+
+
+(define chatbot-remove-dup (lambda (result id-list flows)
+                             (if (null? flows)
+                                 result
+                                 (if (member (flow-get-id (car flows)) id-list)
+                                     (chatbot-remove-dup result id-list (cdr flows))
+                                     (chatbot-remove-dup (append result (list (car flows)))
+                                                         (cons (flow-get-id (car flows)) id-list)
+                                                         (cdr flows))))))
+                                                                     
+
+(define chatbot (lambda (chatbotID name welcomeMessage startFlowId . flows)
+                  (list chatbotID name welcomeMessage startFlowId
+                        (chatbot-remove-dup (list) (list) flows))))
+
+(define chatbot-get-id (lambda (chatbot)
+                         (car chatbot)))
+
+(define chatbot-get-name (lambda (chatbot)
+                         (cadr chatbot)))
+
+(define chatbot-get-msg (lambda (chatbot)
+                         (caddr chatbot)))
+
+(define chatbot-get-startFlowId (lambda (chatbot)
+                         (cadddr chatbot)))
+
+(define chatbot-get-flows (lambda (chatbot)
+                            (car (cddddr chatbot))))
+;cambiar car cddddr por un list ref
+(define chatbot-add-flow (lambda (chatbot flow)
+               (list (chatbot-get-id chatbot) (chatbot-get-name chatbot)
+                     (chatbot-get-msg chatbot) (chatbot-get-startFlowId chatbot)
+                     (chatbot-remove-dup (chatbot-get-flows chatbot)
+                                         (map flow-get-id (chatbot-get-flows chatbot)) (list flow)))))
